6c13d42f04d393343239434ab5024ef590c8750c
shash <34834756+shash64@users.noreply.github.com>
2024-12-15 01:29:59 +0100

    Update fileManager.py

--- fileManager.py
+++ fileManager.py
@@ -1,6 +1,6 @@
 def split_file(data, chunk_size):
     chunks = [data[i:i + chunk_size] for i in range(0, len(data), chunk_size)]
-    print(f"Fichier divisé en {len(chunks)} morceaux.")
+    print(f"Fichier divisé en {len(chunks)} morceaux")
     return chunks
 
 def combine_chunks(chunks):
