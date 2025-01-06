8552437abbd990d20dff6f9eb739f196fbbd1f1b
shash <34834756+shash64@users.noreply.github.com>
2025-01-07 00:21:16 +0100

    Update fileManager.py
    Add split_file() parameters to handle file cutting problems

--- fileManager.py
+++ fileManager.py
@@ -1,8 +1,13 @@
-def split_file(data, chunk_size):
-    chunks = [data[i:i + chunk_size] for i in range(0, len(data), chunk_size)]
-    print(f"Fichier divisé en {len(chunks)} morceaux")
+def split_file(data, num_chunks, chunk_size, remaining):
+    chunks = []
+    start = 0
+    for i in range(num_chunks):
+        end = start + chunk_size + (1 if i<remaining else 0)
+        chunks.append(data[start:end])
+        start = end
     return chunks
 
 def combine_chunks(chunks):
+    total_size = sum(len(chunk) for chunk in chunks)
+    print(f"Taille totale des morceaux: {total_size} octets")
     return b"".join(chunks)
-
